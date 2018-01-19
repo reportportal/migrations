3e7bd1c101a0bdbd26b25e19923f303f108ca6ac
Pavel_Bortnik <pavel_bortnik@epam.com>
2018-01-19 18:40:40 +0300

    introduce activity table

--- migrations/1_initialize_schema.up.sql
+++ migrations/1_initialize_schema.up.sql
@@ -69,7 +69,7 @@ CREATE TABLE project (
 
 
 CREATE TABLE profile (
-  id                 SERIAL CONSTRAINT user_pk PRIMARY KEY,
+  id                 SERIAL CONSTRAINT profile_pk PRIMARY KEY,
   login              VARCHAR        NOT NULL UNIQUE,
   password           VARCHAR        NOT NULL,
   email              VARCHAR        NOT NULL,
@@ -82,10 +82,21 @@ CREATE TABLE profile (
   metadata           JSONB          NULL
 );
 
-CREATE TABLE profile_project(
-  profile_id INT REFERENCES profile (id) ON UPDATE CASCADE ON DELETE CASCADE ,
-  project_id INT REFERENCES project (id) ON UPDATE CASCADE ON DELETE CASCADE ,
+CREATE TABLE profile_project (
+  profile_id   INT REFERENCES profile (id) ON UPDATE CASCADE ON DELETE CASCADE,
+  project_id   INT REFERENCES project (id) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT profile_project_pk PRIMARY KEY (profile_id, project_id),
   project_role PROJECT_ROLE_ENUM NOT NULL
   -- proposed role ??
 );
+
+CREATE TABLE activity (
+  id            SERIAL CONSTRAINT activity_pk PRIMARY KEY,
+  profile_id    INT REFERENCES profile (id) ON DELETE CASCADE,
+  project_id    INT REFERENCES project (id) ON DELETE CASCADE,
+  last_modified TIMESTAMP DEFAULT now() NOT NULL,
+  object_type   VARCHAR                 NOT NULL,
+  action_type   VARCHAR                 NOT NULL,
+  name          VARCHAR                 NOT NULL
+  -- history ??
+)
