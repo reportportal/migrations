066b2a55ca781806b7010a9f6a600c0d051a7060
Pavel_Bortnik <pavel_bortnik@epam.com>
2018-07-12 18:17:50 +0300

    fix missed currval for insert

--- migrations/2_default_data.up.sql
+++ migrations/2_default_data.up.sql
@@ -15,7 +15,8 @@ INSERT INTO project_configuration (id, project_type, interrupt_timeout, keep_log
 VALUES ((SELECT currval(pg_get_serial_sequence('project', 'id'))), 'PERSONAL', '1 day', '1 month', '2 weeks');
 
 INSERT INTO users (login, password, email, role, type, default_project_id, full_name)
-VALUES ('superadmin', '5d39d85bddde885f6579f8121e11eba2', 'superadminemail@domain.com', 'ADMINISTRATOR', 'INTERNAL', 1, 'tester');
+VALUES ('superadmin', '5d39d85bddde885f6579f8121e11eba2', 'superadminemail@domain.com', 'ADMINISTRATOR', 'INTERNAL',
+        (SELECT currval(pg_get_serial_sequence('project', 'id'))), 'tester');
 
 INSERT INTO project_user (user_id, project_id, project_role) VALUES
   ((SELECT currval(pg_get_serial_sequence('users', 'id'))), (SELECT currval(pg_get_serial_sequence('project', 'id'))), 'PROJECT_MANAGER');
