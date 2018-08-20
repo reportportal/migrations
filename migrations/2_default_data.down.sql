bd34e7914e4266465416d55c20d0e09e724e5566
pbortnik <rrrizon@gmail.com>
2018-08-20 17:11:51 +0300

    clear initial scripts

--- migrations/2_default_data.down.sql
+++ migrations/2_default_data.down.sql
@@ -1,11 +1,2 @@
-DELETE FROM project
-WHERE project.name = 'default_personal';
-
-DELETE FROM project
-WHERE project.name = 'superadmin_personal';
-
-DELETE FROM users
-WHERE login = 'default';
-
-DELETE FROM users
-WHERE login = 'superadmin';
+DELETE FROM project;
+DELETE FROM users;
