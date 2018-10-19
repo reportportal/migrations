df01929e624674a05d40d546df0b2d913ccc3570
ivan_nikitsenka <inikitsenka@cloudpassage.com>
2018-10-19 15:01:41 +0300

    add spring acl security tables

--- migrations/1_initialize_schema.up.sql
+++ migrations/1_initialize_schema.up.sql
@@ -486,44 +486,44 @@ CREATE TABLE issue_ticket (
 
 ------------------------------ ACL Security --------------------------------------------
 
-create table acl_sid(
-    id bigserial not null primary key,
-    principal boolean not null,
-    sid varchar(100) not null,
-    constraint unique_uk_1 unique(sid,principal)
+CREATE TABLE acl_sid(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    principal BOOLEAN NOT NULL,
+    sid VARCHAR(100) NOT NULL,
+    CONSTRAINT unique_uk_1 UNIQUE(sid,principal)
 );
 
-create table acl_class(
-    id bigserial not null primary key,
-    class varchar(100) not null,
-    constraint unique_uk_2 unique(class)
+CREATE TABLE acl_class(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    class VARCHAR(100) NOT NULL,
+    CONSTRAINT unique_uk_2 UNIQUE(class)
 );
 
-create table acl_object_identity(
-    id bigserial primary key,
-    object_id_class bigint not null,
-    object_id_identity varchar(36) not null,
-    parent_object bigint,
-    owner_sid bigint,
-    entries_inheriting boolean not null,
-    constraint unique_uk_3 unique(object_id_class,object_id_identity),
-    constraint foreign_fk_1 foreign key(parent_object)references acl_object_identity(id),
-    constraint foreign_fk_2 foreign key(object_id_class)references acl_class(id),
-    constraint foreign_fk_3 foreign key(owner_sid)references acl_sid(id)
+CREATE TABLE acl_object_identity(
+    id BIGSERIAL PRIMARY KEY,
+    object_id_class BIGINT NOT NULL,
+    object_id_identity VARCHAR(36) NOT NULL,
+    parent_object BIGINT,
+    owner_sid BIGINT,
+    entries_inheriting BOOLEAN NOT NULL,
+    CONSTRAINT unique_uk_3 UNIQUE(object_id_class,object_id_identity),
+    CONSTRAINT foreign_fk_1 FOREIGN KEY(parent_object)REFERENCES acl_object_identity(id),
+    CONSTRAINT foreign_fk_2 FOREIGN KEY(object_id_class)REFERENCES acl_class(id),
+    CONSTRAINT foreign_fk_3 FOREIGN KEY(owner_sid)REFERENCES acl_sid(id)
 );
 
-create table acl_entry(
-    id bigserial primary key,
-    acl_object_identity bigint not null,
-    ace_order int not null,
-    sid bigint not null,
-    mask integer not null,
-    granting boolean not null,
-    audit_success boolean not null,
-    audit_failure boolean not null,
-    constraint unique_uk_4 unique(acl_object_identity,ace_order),
-    constraint foreign_fk_4 foreign key(acl_object_identity) references acl_object_identity(id),
-    constraint foreign_fk_5 foreign key(sid) references acl_sid(id)
+CREATE TABLE acl_entry(
+    id BIGSERIAL PRIMARY KEY,
+    acl_object_identity BIGINT NOT NULL,
+    ace_order int NOT NULL,
+    sid BIGINT NOT NULL,
+    mask INTEGER NOT NULL,
+    granting BOOLEAN NOT NULL,
+    audit_success BOOLEAN NOT NULL,
+    audit_failure BOOLEAN NOT NULL,
+    CONSTRAINT unique_uk_4 UNIQUE(acl_object_identity,ace_order),
+    CONSTRAINT foreign_fk_4 FOREIGN KEY(acl_object_identity) REFERENCES acl_object_identity(id),
+    CONSTRAINT foreign_fk_5 FOREIGN KEY(sid) REFERENCES acl_sid(id)
 );
 
 ----------------------------------------------------------------------------------------
