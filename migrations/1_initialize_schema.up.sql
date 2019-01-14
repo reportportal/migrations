5459e3c15294dbf137a0357928e6665d4ad16245
yumfriez <budaevqwerty@gmail.com>
2019-01-14 17:50:57 +0300

    Nullifying default project id in the users table after project removing

--- migrations/1_initialize_schema.up.sql
+++ migrations/1_initialize_schema.up.sql
@@ -68,7 +68,7 @@ CREATE TABLE users (
   role                 VARCHAR NOT NULL,
   type                 VARCHAR NOT NULL,
   expired              BOOLEAN NOT NULL,
-  default_project_id   BIGINT REFERENCES project (id),
+  default_project_id   BIGINT REFERENCES project (id) ON DELETE SET NULL,
   full_name            VARCHAR NOT NULL,
   metadata             JSONB   NULL
 );
