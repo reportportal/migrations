a925fa40d3f1074d7255d8ccbd488a9e72ab07d1
yumfriez <budaevqwerty@gmail.com>
2019-04-29 17:00:47 +0300

    Notifications disabled by default param update

--- migrations/3_default_data.up.sql
+++ migrations/3_default_data.up.sql
@@ -84,7 +84,7 @@ BEGIN
     INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (9, FALSE, defaultproject), (9, FALSE, superadminproject);
     INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (10, FALSE, defaultproject), (10, FALSE, superadminproject);
     INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (11, 'LAUNCH_NAME', defaultproject), (11, 'LAUNCH_NAME', superadminproject);
-    INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (12, 'true', defaultproject), (12, 'true', superadminproject);
+    INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (12, 'false', defaultproject), (12, 'false', superadminproject);
     INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (13, 'reportportal@example.com', defaultproject), (13, 'reportportal@example.com', superadminproject);
     INSERT INTO project_attribute (attribute_id, value, project_id) VALUES (14, 'false', defaultproject), (14, 'false', superadminproject);
 
