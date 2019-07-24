b05311ee801e065ba70c485cce84dbae8addf3b3
pavel_bortnik <pavel_bortnik@epam.com>
2019-07-24 18:34:14 +0300

    refactor launch owner logic

--- migrations/1_initialize_schema.up.sql
+++ migrations/1_initialize_schema.up.sql
@@ -412,19 +412,19 @@ CREATE TABLE launch
     CONSTRAINT launch_pk PRIMARY KEY,
   uuid                 VARCHAR(36)                                      NOT NULL UNIQUE,
   project_id           BIGINT REFERENCES project (id) ON DELETE CASCADE NOT NULL,
-  user_id              BIGINT                                           REFERENCES users (id) ON DELETE SET NULL,
+  owner                VARCHAR                                          NOT NULL,
   name                 VARCHAR(256)                                     NOT NULL,
   description          TEXT,
   start_time           TIMESTAMP                                        NOT NULL,
   end_time             TIMESTAMP,
   number               INTEGER                                          NOT NULL,
-  last_modified        TIMESTAMP                                                 DEFAULT now() NOT NULL,
+  last_modified        TIMESTAMP DEFAULT now()                          NOT NULL,
   mode                 LAUNCH_MODE_ENUM                                 NOT NULL,
   status               STATUS_ENUM                                      NOT NULL,
   has_retries          BOOLEAN                                          NOT NULL DEFAULT FALSE,
   rerun                BOOLEAN                                          NOT NULL DEFAULT FALSE,
   approximate_duration DOUBLE PRECISION                                          DEFAULT 0.0,
-  CONSTRAINT unq_name_number UNIQUE (name, number, project_id, uuid)
+  CONSTRAINT unq_name_number UNIQUE (name, number, project_id)
 );
 
 CREATE INDEX launch_project_idx
@@ -1048,25 +1048,6 @@ END;
 $$
   LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION get_last_launch_number()
-  RETURNS TRIGGER AS
-$BODY$
-BEGIN
-  new.number = (SELECT number
-                FROM launch
-                WHERE name = new.name
-                  AND project_id = new.project_id
-                ORDER BY number DESC
-                LIMIT 1) + 1;
-  new.number = CASE
-                 WHEN new.number IS NULL
-                   THEN 1
-                 ELSE new.number END;
-  RETURN new;
-END;
-$BODY$
-  LANGUAGE plpgsql;
-
 
 CREATE OR REPLACE FUNCTION count_approximate_duration()
   RETURNS TRIGGER AS
@@ -1110,13 +1091,6 @@ CREATE TRIGGER after_ticket_delete
   ON issue_ticket
   FOR EACH ROW
 EXECUTE PROCEDURE check_wired_tickets();
-
-
-CREATE TRIGGER last_launch_number_trigger
-  BEFORE INSERT
-  ON launch
-  FOR EACH ROW
-EXECUTE PROCEDURE get_last_launch_number();
 
 CREATE TRIGGER approximate_duration_trigger
   BEFORE INSERT
