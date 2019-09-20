69beb9e75014b95d1d4aa75c7b92e47fc1eaf367
pavel_bortnik <pavel_bortnik@epam.com>
2019-09-20 19:03:28 +0300

    added default statistics field in up script

--- migrations/3_default_data.up.sql
+++ migrations/3_default_data.up.sql
@@ -17,17 +17,38 @@ BEGIN
     INSERT INTO integration_type (enabled, name, creation_date, group_type) VALUES (TRUE, 'email', now(), 'NOTIFICATION');
     email := (SELECT currval(pg_get_serial_sequence('integration_type', 'id')));
 
-    INSERT INTO issue_group (issue_group_id, issue_group) VALUES (1, 'TO_INVESTIGATE');
-    INSERT INTO issue_group (issue_group_id, issue_group) VALUES (2, 'AUTOMATION_BUG');
-    INSERT INTO issue_group (issue_group_id, issue_group) VALUES (3, 'PRODUCT_BUG');
-    INSERT INTO issue_group (issue_group_id, issue_group) VALUES (4, 'NO_DEFECT');
-    INSERT INTO issue_group (issue_group_id, issue_group) VALUES (5, 'SYSTEM_ISSUE');
+    INSERT INTO issue_group (issue_group_id, issue_group) VALUES (1, 'TO_INVESTIGATE'),
+                                                                 (2, 'AUTOMATION_BUG'),
+                                                                 (3, 'PRODUCT_BUG'),
+                                                                 (4, 'NO_DEFECT'),
+                                                                 (5, 'SYSTEM_ISSUE');
+
+    ALTER SEQUENCE issue_group_issue_group_id_seq RESTART WITH 6;
 
     INSERT INTO issue_type (issue_group_id, locator, issue_name, abbreviation, hex_color) VALUES (1, 'ti001', 'To Investigate', 'TI', '#ffb743');
     INSERT INTO issue_type (issue_group_id, locator, issue_name, abbreviation, hex_color) VALUES (2, 'ab001', 'Automation Bug', 'AB', '#f7d63e');
     INSERT INTO issue_type (issue_group_id, locator, issue_name, abbreviation, hex_color) VALUES (3, 'pb001', 'Product Bug', 'PB', '#ec3900');
     INSERT INTO issue_type (issue_group_id, locator, issue_name, abbreviation, hex_color) VALUES (4, 'nd001', 'No Defect', 'ND', '#777777');
     INSERT INTO issue_type (issue_group_id, locator, issue_name, abbreviation, hex_color) VALUES (5, 'si001', 'System Issue', 'SI', '#0274d1');
+
+    ALTER SEQUENCE issue_type_id_seq RESTART WITH 6;
+
+    INSERT INTO statistics_field (sf_id, name) VALUES (1, 'statistics$executions$total'),
+                                                      (2, 'statistics$executions$passed'),
+                                                      (3, 'statistics$executions$skipped'),
+                                                      (4, 'statistics$executions$failed'),
+                                                      (5, 'statistics$defects$automation_bug$total'),
+                                                      (6, 'statistics$defects$automation_bug$ab001'),
+                                                      (7, 'statistics$defects$product_bug$total'),
+                                                      (8, 'statistics$defects$product_bug$pb001'),
+                                                      (9, 'statistics$defects$system_issue$total'),
+                                                      (10, 'statistics$defects$system_issue$si001'),
+                                                      (11, 'statistics$defects$to_investigate$total'),
+                                                      (12, 'statistics$defects$to_investigate$ti001'),
+                                                      (13, 'statistics$defects$no_defect$total'),
+                                                      (14, 'statistics$defects$no_defect$nd001');
+
+    ALTER SEQUENCE statistics_field_sf_id_seq RESTART WITH 15;
 
     INSERT INTO attribute (name) VALUES ('job.interruptJobTime');
     INSERT INTO attribute (name) VALUES ('job.keepLaunches');
