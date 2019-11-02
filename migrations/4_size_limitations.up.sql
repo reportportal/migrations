e33b3f40279dafa8305164f08ed9cffd7979595f
pavel_bortnik <pavel_bortnik@epam.com>
2019-11-02 22:15:58 +0300

    add multi next val

--- migrations/4_size_limitations.up.sql
+++ migrations/4_size_limitations.up.sql
@@ -5,3 +5,18 @@ ALTER TABLE ticket
 
 ALTER TABLE test_item
     ALTER COLUMN unique_id TYPE VARCHAR(1024);
+
+CREATE OR REPLACE FUNCTION multi_nextval(use_seqname REGCLASS,
+                                         use_increment INTEGER) RETURNS BIGINT AS
+$$
+DECLARE
+    reply   BIGINT;
+    lock_id BIGINT := (use_seqname::BIGINT - 2147483648)::INTEGER;
+BEGIN
+    PERFORM pg_advisory_lock(lock_id);
+    reply := nextval(use_seqname);
+    PERFORM setval(use_seqname, reply + use_increment - 1, TRUE);
+    PERFORM pg_advisory_unlock(lock_id);
+    RETURN reply;
+END;
+$$ LANGUAGE plpgsql;
