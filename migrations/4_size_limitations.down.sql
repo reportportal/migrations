3ff63926df5b8c407853f44b92aaae6ec64c512a
pavel_bortnik <pavel_bortnik@epam.com>
2019-11-28 18:27:45 +0300

    added activities indexes

--- migrations/4_size_limitations.down.sql
+++ migrations/4_size_limitations.down.sql
@@ -11,6 +11,5 @@ DROP INDEX IF EXISTS log_attach_id_idx;
 
 DROP INDEX IF EXISTS log_launch_id_idx;
 
-DROP INDEX IF EXISTS activity_project_idx;
-CREATE INDEX IF NOT EXISTS activity_project_idx
-    ON activity (project_id);
+DROP INDEX IF EXISTS activity_creation_date_idx;
+DROP INDEX IF EXISTS activity_object_idx;
--- migrations/4_size_limitations.up.sql
+++ migrations/4_size_limitations.up.sql
@@ -16,6 +16,10 @@ DROP INDEX IF EXISTS log_launch_id_idx;
 CREATE INDEX IF NOT EXISTS log_launch_id_idx
     ON log (launch_id);
 
-DROP INDEX IF EXISTS activity_project_idx;
-CREATE INDEX IF NOT EXISTS activity_project_idx
-    ON activity (project_id, creation_date);
+DROP INDEX IF EXISTS activity_creation_date_idx;
+CREATE INDEX IF NOT EXISTS activity_creation_date_idx
+    ON activity (creation_date);
+
+DROP INDEX IF EXISTS activity_object_idx;
+CREATE INDEX IF NOT EXISTS activity_object_idx
+    ON activity (object_id);
