b09ceb2ccc7f6368725f7bb1c95fb0959532f8fe
yumfriez <budaevqwerty@gmail.com>
2019-12-09 13:23:40 +0300

    Test case id hash migration fix

--- migrations/5_test_case_id_type.down.sql
+++ migrations/5_test_case_id_type.down.sql
@@ -1,4 +1,8 @@
+UPDATE test_item
+SET test_case_id=test_case_hash::VARCHAR;
+
 ALTER TABLE test_item
     ALTER COLUMN test_case_id TYPE INTEGER USING test_case_id::INTEGER;
+
 ALTER TABLE test_item
     DROP COLUMN test_case_hash;
--- migrations/5_test_case_id_type.up.sql
+++ migrations/5_test_case_id_type.up.sql
@@ -1,8 +1,11 @@
 ALTER TABLE test_item
-    ALTER COLUMN test_case_id TYPE VARCHAR(256);
+    ADD COLUMN test_case_hash INTEGER;
+
+UPDATE test_item
+SET test_case_hash=test_case_id;
 
 ALTER TABLE test_item
-    ADD COLUMN test_case_hash INTEGER;
+    ALTER COLUMN test_case_id TYPE VARCHAR(256);
 
 CREATE INDEX test_case_hash_idx ON test_item (test_case_hash);
 CREATE INDEX test_case_hash_launch_id_idx ON test_item (test_case_hash, launch_id);
