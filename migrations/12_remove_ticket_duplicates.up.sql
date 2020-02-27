a7d86ae50cb1c59c3a5af298b36bcaa6f9291959
pavel_bortnik <pavel_bortnik@epam.com>
2020-02-27 13:43:36 +0300

    fix synth

--- migrations/12_remove_ticket_duplicates.up.sql
+++ migrations/12_remove_ticket_duplicates.up.sql
@@ -1,4 +1,4 @@
-CREATE INDEX ticket_id_idx IF NOT EXISTS ON ticket(ticket_id);
+CREATE INDEX IF NOT EXISTS ticket_id_idx ON ticket(ticket_id);
 CREATE OR REPLACE FUNCTION migrate_tickets() RETURNS VOID AS
 $$
 DECLARE
