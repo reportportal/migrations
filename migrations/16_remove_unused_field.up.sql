04bf99de94a157caae88fc229c13aaebebe0e40c
pavel_bortnik <pavel_bortnik@epam.com>
2020-04-15 16:28:27 +0300

    update index removing

--- migrations/16_remove_unused_field.up.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE log DROP COLUMN uuid;
-
-DROP INDEX IF EXISTS launch_uuid_idx;
-CREATE INDEX IF NOT EXISTS launch_uuid_idx ON launch (uuid);
-
-DROP INDEX IF EXISTS ti_uuid_idx;
-CREATE INDEX IF NOT EXISTS ti_uuid_idx ON test_item (uuid);
