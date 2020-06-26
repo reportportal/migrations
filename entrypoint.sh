af2934747a042ef1c7851dfe86de4ee6825224ec
Jeremy Lundy <jeremy.lundy@litmus.io>
2020-06-26 14:09:46 -0400

    Get Postgres password from file in case of swarm deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+if [ ! -z $POSTGRES_PASSWORD_FILE ]; then
+  export POSTGRES_PASSWORD=$(cat $POSTGRES_PASSWORD_FILE)
+fi
+
 exec /wait-for-it.sh $POSTGRES_SERVER:$POSTGRES_PORT -- migrate -path /migrations -database postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER/$POSTGRES_DB?sslmode=disable "$@"
