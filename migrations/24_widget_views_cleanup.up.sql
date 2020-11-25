f29889ec539299dc89854ae2525de126fb7902ba
pavel_bortnik <pavel_bortnik@epam.com>
2020-11-25 18:08:00 +0300

    Add views clean up of not existed widgets

--- migrations/24_widget_views_cleanup.up.sql
+++ migrations/24_widget_views_cleanup.up.sql
@@ -1,4 +1,4 @@
-DELETE
+PREPARE clean_up as SELECT 'DROP MATERIALIZED VIEW ' || string_agg(matviewname, ', ')
 FROM pg_matviews
 WHERE matviewname LIKE 'widget_%'
    OR matviewname LIKE 'hct_%' AND matviewname
@@ -7,3 +7,4 @@ WHERE matviewname LIKE 'widget_%'
                                     FROM widget
                                     WHERE widget_type IN ('componentHealthCheckTable', 'cumulative')
                                       AND widget_options -> 'options' ->> 'viewName' NOTNULL);
+EXECUTE clean_up;
