4baa2acc023405bfbd595b0c203eec44aa8faa07
pavel_bortnik <pavel_bortnik@epam.com>
2020-11-25 18:17:19 +0300

    Add views clean up of not existed widgets

--- migrations/24_widget_views_cleanup.up.sql
+++ migrations/24_widget_views_cleanup.up.sql
@@ -1,10 +1,22 @@
-PREPARE clean_up as SELECT 'DROP MATERIALIZED VIEW ' || string_agg(matviewname, ', ')
-FROM pg_matviews
-WHERE matviewname LIKE 'widget_%'
-   OR matviewname LIKE 'hct_%' AND matviewname
-    NOT IN
-                                   (SELECT widget_options -> 'options' ->> 'viewName'
-                                    FROM widget
-                                    WHERE widget_type IN ('componentHealthCheckTable', 'cumulative')
-                                      AND widget_options -> 'options' ->> 'viewName' NOTNULL);
-EXECUTE clean_up;
+CREATE OR REPLACE FUNCTION clean_up_views()
+AS
+$$
+DECLARE
+    drop_query VARCHAR;
+BEGIN
+    drop_query := (SELECT 'DROP MATERIALIZED VIEW ' || string_agg(matviewname, ', ')
+                   FROM pg_matviews
+                   WHERE matviewname LIKE 'widget_%'
+                      OR matviewname LIKE 'hct_%' AND matviewname
+                       NOT IN
+                                                      (SELECT widget_options -> 'options' ->> 'viewName'
+                                                       FROM widget
+                                                       WHERE widget_type IN ('componentHealthCheckTable', 'cumulative')
+                                                         AND widget_options -> 'options' ->> 'viewName' NOTNULL));
+    EXECUTE drop_query;
+END;
+$$
+    LANGUAGE plpgsql;
+
+SELECT clean_up_views();
+DROP FUNCTION IF EXISTS clean_up_views;
