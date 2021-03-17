76a8a782c31bb5d3ff434ca549fd83b984842e28
Ivan Budayeu <ivan_budayeu@epam.com>
2021-03-17 11:56:08 +0300

    Index creation provided

--- migrations/27_alter_log_attachment.up.sql
+++ migrations/27_alter_log_attachment.up.sql
@@ -1,5 +1,7 @@
 ALTER TABLE log
     ADD COLUMN project_id BIGINT REFERENCES project (id);
+CREATE INDEX log_project_id_idx ON log (project_id);
 
 ALTER TABLE attachment
     ADD COLUMN creation_date TIMESTAMP;
+CREATE INDEX attach_creation_date_idx ON attachment (creation_date);
