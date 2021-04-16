b6b95d5b23f7a7ad275419fecee4d97e389cd99a
Pavel_Bortnik <pavel_bortnik@epam.com>
2021-04-16 13:22:50 +0300

    Add index for launch jobs

--- migrations/34_add_jobs_indexes.up.sql
+++ migrations/34_add_jobs_indexes.up.sql
@@ -3,3 +3,6 @@ CREATE INDEX IF NOT EXISTS log_project_id_log_time_idx
 
 CREATE INDEX IF NOT EXISTS attachment_project_id_creation_time_idx
     ON attachment (project_id, creation_date);
+
+DROP INDEX IF EXISTS launch_project_idx;
+CREATE INDEX IF NOT EXISTS launch_project_start_time_idx ON launch (project_id, start_time);
