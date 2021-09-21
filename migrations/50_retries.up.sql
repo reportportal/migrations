19b23e0e427805a7b4989fdf06032b2094699f03
Maksim Antonov <Maksim_Antonov@epam.com>
2021-09-21 19:23:07 +0300

    Update statistic calculation triggers

--- migrations/50_retries.up.sql
+++ migrations/50_retries.up.sql
@@ -1,3 +1,249 @@
+CREATE OR REPLACE FUNCTION decrease_statistics()
+    RETURNS TRIGGER AS
+$$
+DECLARE
+    cur_launch_id         BIGINT;
+    DECLARE
+    cur_statistics_fields RECORD;
+    DECLARE
+    processed_test_item RECORD;
+BEGIN
+
+    SELECT item_id, launch_id, has_stats, retry_of, ('{' || replace(path::TEXT, '.', ',') || '}')::bigint[] AS arr_path INTO processed_test_item FROM test_item WHERE item_id = old.result_id FOR UPDATE;
+
+    cur_launch_id := processed_test_item.launch_id;
+    IF cur_launch_id IS NULL
+    THEN
+        RETURN old;
+    END IF;
+
+    IF NOT processed_test_item.has_stats OR processed_test_item.retry_of IS NOT NULL
+    THEN
+        RETURN old;
+    END IF;
+
+    FOR cur_statistics_fields IN (SELECT statistics_field_id, s_counter
+                                  FROM statistics
+                                  WHERE item_id = old.result_id
+                                  ORDER BY statistics_field_id)
+        LOOP
+            UPDATE statistics
+            SET s_counter = s_counter - cur_statistics_fields.s_counter
+            WHERE statistics.statistics_field_id = cur_statistics_fields.statistics_field_id
+              AND (launch_id = cur_launch_id
+                OR
+                   (item_id = ANY(processed_test_item.arr_path) AND item_id != old.result_id)
+                );
+        END LOOP;
+
+    DELETE FROM statistics WHERE item_id = old.result_id;
+
+    RETURN old;
+END;
+$$
+    LANGUAGE plpgsql;
+
+--------------------------------------------
+
+CREATE OR REPLACE FUNCTION update_defect_statistics()
+    RETURNS TRIGGER AS
+$$
+DECLARE
+    defect_field              VARCHAR;
+    DECLARE
+    defect_field_total        VARCHAR;
+    DECLARE
+    defect_field_old_id       BIGINT;
+    DECLARE
+    defect_field_old_total_id BIGINT;
+    DECLARE
+    defect_field_id           BIGINT;
+    DECLARE
+    defect_field_total_id     BIGINT;
+    DECLARE
+    cur_launch_id             BIGINT;
+    DECLARE
+    processed_test_item RECORD;
+BEGIN
+    IF old.issue_type = new.issue_type
+    THEN
+        RETURN new;
+    END IF;
+
+    SELECT item_id, launch_id, has_stats, retry_of, ('{' || replace(path::TEXT, '.', ',') || '}')::bigint[] AS arr_path INTO processed_test_item FROM test_item WHERE item_id = new.issue_id FOR UPDATE;
+
+    cur_launch_id := processed_test_item.launch_id;
+    IF cur_launch_id IS NULL
+    THEN
+        RETURN new;
+    END IF;
+
+    IF NOT processed_test_item.has_stats OR processed_test_item.retry_of IS NOT NULL
+    THEN
+        RETURN new;
+    END IF;
+
+    IF exists(SELECT 1
+              FROM test_item AS parent
+                       JOIN test_item AS child ON parent.item_id = child.parent_id
+              WHERE (parent.item_id = new.issue_id AND child.has_stats)
+        )
+    THEN
+        RETURN new;
+    END IF;
+
+    defect_field := (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$',
+                                   lower(public.issue_type.locator))
+                     FROM issue_type JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
+                     WHERE issue_type.id = new.issue_type);
+
+    defect_field_old_id := (SELECT sf_id FROM statistics_field
+                            WHERE statistics_field.name =
+                                  (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$',
+                                                 lower(public.issue_type.locator))
+                                   FROM issue_type JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
+                                   WHERE issue_type.id = old.issue_type) LIMIT 1);
+
+    defect_field_total := (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$total')
+                           FROM issue_type JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
+                           WHERE issue_type.id = new.issue_type);
+
+    defect_field_old_total_id := (SELECT sf_id
+                                  FROM statistics_field
+                                  WHERE statistics_field.name =
+                                        (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$total')
+                                         FROM issue_type JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
+                                         WHERE issue_type.id = old.issue_type) LIMIT 1);
+
+    defect_field_id = (SELECT sf_id FROM statistics_field WHERE statistics_field.name = defect_field LIMIT 1);
+    IF defect_field_id IS NULL
+    THEN
+        INSERT INTO statistics_field (name) VALUES (defect_field) ON CONFLICT DO NOTHING;
+        defect_field_id = (SELECT sf_id FROM statistics_field WHERE statistics_field.name = defect_field LIMIT 1);
+    END IF;
+
+    defect_field_total_id = (SELECT sf_id FROM statistics_field WHERE statistics_field.name = defect_field_total LIMIT 1);
+    IF defect_field_total_id IS NULL
+    THEN
+        INSERT INTO statistics_field (name) VALUES (defect_field_total) ON CONFLICT DO NOTHING;
+        defect_field_total_id = (SELECT sf_id FROM statistics_field WHERE statistics_field.name = defect_field_total LIMIT 1);
+    END IF;
+
+    /* decrease item defects statistics for concrete field */
+    UPDATE statistics SET s_counter = s_counter - 1
+    WHERE statistics_field_id = defect_field_old_id
+      AND statistics.item_id = ANY(processed_test_item.arr_path);
+
+    /* increment item defects statistics for concrete field */
+    INSERT INTO statistics (s_counter, statistics_field_id, item_id)
+    VALUES (1, defect_field_id, unnest(processed_test_item.arr_path))
+    ON CONFLICT (statistics_field_id,item_id) DO UPDATE SET s_counter = statistics.s_counter + 1;
+
+    /* decrease item defects statistics for total field */
+    UPDATE statistics
+    SET s_counter = s_counter - 1
+    WHERE statistics_field_id = defect_field_old_total_id
+      AND item_id = ANY(processed_test_item.arr_path);
+
+    /* increment item defects statistics for total field */
+    INSERT INTO statistics (s_counter, statistics_field_id, item_id)
+    VALUES (1, defect_field_total_id, unnest(processed_test_item.arr_path))
+    ON CONFLICT (statistics_field_id,item_id) DO UPDATE SET s_counter = statistics.s_counter + 1;
+
+    /* decrease launch defects statistics for concrete field */
+    UPDATE statistics SET s_counter = s_counter - 1
+    WHERE statistics_field_id = defect_field_old_id AND launch_id = cur_launch_id;
+
+    /* increment launch defects statistics for concrete field */
+    INSERT INTO statistics (s_counter, statistics_field_id, launch_id)
+    VALUES (1, defect_field_id, cur_launch_id)
+    ON CONFLICT (statistics_field_id,launch_id) DO UPDATE SET s_counter = statistics.s_counter + 1;
+
+    /* decrease launch defects statistics for total field */
+    UPDATE statistics SET s_counter = s_counter - 1
+    WHERE statistics_field_id = defect_field_old_total_id AND launch_id = cur_launch_id;
+
+    /* increment launch defects statistics for total field */
+    INSERT INTO statistics (s_counter, statistics_field_id, launch_id)
+    VALUES (1, defect_field_total_id, cur_launch_id)
+    ON CONFLICT (statistics_field_id,launch_id) DO UPDATE SET s_counter = statistics.s_counter + 1;
+    RETURN new;
+END;
+$$
+    LANGUAGE plpgsql;
+
+---------------------------------------------
+
+CREATE OR REPLACE FUNCTION delete_defect_statistics()
+    RETURNS TRIGGER AS
+$$
+DECLARE
+    DECLARE
+    cur_launch_id             BIGINT;
+    DECLARE
+    defect_field_old_id       BIGINT;
+    DECLARE
+    defect_field_old_total_id BIGINT;
+    DECLARE
+    processed_test_item RECORD;
+BEGIN
+    SELECT item_id, launch_id, has_stats, retry_of, ('{' || replace(path::TEXT, '.', ',') || '}')::bigint[] AS arr_path
+    INTO processed_test_item FROM test_item WHERE item_id = old.issue_id FOR UPDATE;
+
+    cur_launch_id := processed_test_item.launch_id;
+    IF cur_launch_id IS NULL
+    THEN
+        RETURN old;
+    END IF;
+
+    IF NOT processed_test_item.has_stats OR processed_test_item.retry_of IS NOT NULL
+    THEN
+        RETURN old;
+    END IF;
+
+    defect_field_old_id := (SELECT sf_id
+                            FROM statistics_field
+                            WHERE statistics_field.name =
+                                  (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$',
+                                                 lower(public.issue_type.locator))
+                                   FROM issue_type
+                                            JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
+                                   WHERE issue_type.id = old.issue_type) LIMIT 1);
+
+    defect_field_old_total_id := (SELECT sf_id
+                                  FROM statistics_field
+                                  WHERE statistics_field.name =
+                                        (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$total')
+                                         FROM issue_type
+                                                  JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
+                                         WHERE issue_type.id = old.issue_type) LIMIT 1);
+
+    /* decrease item defects statistics for concrete field */
+    UPDATE statistics
+    SET s_counter = s_counter - 1
+    WHERE statistics_field_id = defect_field_old_id AND statistics.item_id = ANY(processed_test_item.arr_path);
+
+    /* decrease item defects statistics for total field */
+    UPDATE statistics
+    SET s_counter = s_counter - 1
+    WHERE statistics_field_id = defect_field_old_total_id AND item_id = ANY(processed_test_item.arr_path);
+
+    /* decrease launch defects statistics for concrete field */
+    UPDATE statistics
+    SET s_counter = s_counter - 1
+    WHERE statistics_field_id = defect_field_old_id AND launch_id = cur_launch_id;
+
+    /* decrease launch defects statistics for total field */
+    UPDATE statistics
+    SET s_counter = s_counter - 1
+    WHERE statistics_field_id = defect_field_old_total_id AND launch_id = cur_launch_id;
+    RETURN old;
+END;
+$$
+    LANGUAGE plpgsql;
+
+------------------------------------------------
+
 CREATE OR REPLACE FUNCTION delete_item_statistics(test_item_id BIGINT)
     RETURNS INTEGER AS
 $$
