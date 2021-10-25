f065d941e35470b2db49e4c30030f5154f44dd77
Ivan_Budayeu <Skileton2018>
2021-10-25 13:41:45 +0300

    Formatting fix

--- migrations/51_cluster.up.sql
+++ migrations/51_cluster.up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS clusters
 (
-    id         BIGINT PRIMARY KEY,
-    launch_id  BIGINT REFERENCES launch (id) ON DELETE CASCADE,
-    message    TEXT         NOT NULL
+    id        BIGINT PRIMARY KEY,
+    launch_id BIGINT REFERENCES launch (id) ON DELETE CASCADE,
+    message   TEXT NOT NULL
 );
 
-ALTER TABLE log ADD COLUMN cluster_id BIGINT REFERENCES clusters(id) ON DELETE SET NULL;
+ALTER TABLE log
+    ADD COLUMN cluster_id BIGINT REFERENCES clusters (id) ON DELETE SET NULL;
+
 CREATE INDEX log_cluster_idx ON log (cluster_id);
