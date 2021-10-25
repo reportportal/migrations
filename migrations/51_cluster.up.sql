4570ae0359719eb31e218ee9bfd3950a41df2c91
Ivan_Budayeu <Skileton2018>
2021-10-25 13:39:52 +0300

    Cluster entity provided. Log table updated with reference

---
+++ migrations/51_cluster.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS clusters
+(
+    id         BIGINT PRIMARY KEY,
+    launch_id  BIGINT REFERENCES launch (id) ON DELETE CASCADE,
+    message    TEXT         NOT NULL
+);
+
+ALTER TABLE log ADD COLUMN cluster_id BIGINT REFERENCES clusters(id) ON DELETE SET NULL;
+CREATE INDEX log_cluster_idx ON log (cluster_id);
