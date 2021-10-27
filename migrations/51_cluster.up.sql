e70111fd118e027ed8b5430360955344c5d49b98
Ivan_Budayeu <Skileton2018>
2021-10-27 12:29:18 +0300

    References removed

--- migrations/51_cluster.up.sql
+++ migrations/51_cluster.up.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS clusters
 (
     id        BIGINT PRIMARY KEY,
-    launch_id BIGINT REFERENCES launch (id),
+    launch_id BIGINT,
     message   TEXT NOT NULL
 );
 
+CREATE INDEX cluster_launch_idx ON clusters(launch_id);
+
 ALTER TABLE log
-    ADD COLUMN cluster_id BIGINT REFERENCES clusters (id) ON DELETE SET NULL;
+    ADD COLUMN cluster_id BIGINT;
 
 CREATE INDEX log_cluster_idx ON log (cluster_id);
