1c086809bc035e3bc6d8a29d66087dea2f48870c
Ivan_Budayeu <Skileton2018>
2021-10-27 13:16:36 +0300

    Project id reference provided

--- migrations/51_cluster.up.sql
+++ migrations/51_cluster.up.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS clusters
 (
-    id        BIGINT PRIMARY KEY,
-    launch_id BIGINT NOT NULL,
-    message   TEXT NOT NULL
+    id         BIGINT PRIMARY KEY,
+    project_id BIGINT NOT NULL,
+    launch_id  BIGINT NOT NULL,
+    message    TEXT   NOT NULL
 );
 
-CREATE INDEX cluster_launch_idx ON clusters(launch_id);
+CREATE INDEX cluster_project_idx ON clusters (project_id);
+CREATE INDEX cluster_launch_idx ON clusters (launch_id);
 
 ALTER TABLE log
     ADD COLUMN cluster_id BIGINT;
