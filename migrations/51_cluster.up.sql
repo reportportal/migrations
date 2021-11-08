1af06e9fc631cbbdbc58c5dbaaa0dc38e5e019bc
Ivan_Budayeu <Skileton2018>
2021-11-08 15:17:45 +0300

    EPMRPP-68409 M2M cluster_test_item table added

--- migrations/51_cluster.up.sql
+++ migrations/51_cluster.up.sql
@@ -13,6 +13,16 @@ CREATE INDEX cluster_index_id_idx ON clusters (index_id);
 CREATE INDEX cluster_project_idx ON clusters (project_id);
 CREATE INDEX cluster_launch_idx ON clusters (launch_id);
 
+CREATE TABLE IF NOT EXISTS clusters_test_item
+(
+    cluster_id BIGINT NOT NULL,
+    item_id    BIGINT NOT NULL,
+    CONSTRAINT cluster_item_unq UNIQUE (cluster_id, item_id)
+);
+
+CREATE INDEX cluster_item_cluster_idx ON clusters_test_item(cluster_id);
+CREATE INDEX cluster_item_item_idx ON clusters_test_item(item_id);
+
 ALTER TABLE log
     ADD COLUMN cluster_id BIGINT;
 
