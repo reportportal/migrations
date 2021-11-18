0cce5b12d1d6320db54b05d2b84d640bb23ba48b
Ivan <budaevqwerty@gmail.com>
2021-11-18 12:19:06 +0300

    EPMRPP-68775 || Auto unique error attributes provided (#209)

---
+++ migrations/54_analyzer_unique_error_attribute.down.sql
@@ -0,0 +1,4 @@
+DELETE FROM project_attribute WHERE attribute_id = 19;
+DELETE FROM attribute WHERE name = 'analyzer.uniqueError.removeNumbers';
+DELETE FROM project_attribute WHERE attribute_id = 18;
+DELETE FROM attribute WHERE name = 'analyzer.uniqueError.removeNumbers';
