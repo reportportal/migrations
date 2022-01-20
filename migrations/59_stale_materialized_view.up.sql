038190fe1a9b81bb5ebad794881ef7bd2c252fb3
Ivan_Budayeu <Skileton2018>
2022-01-20 13:17:45 +0300

    EPMRPP-73239 || Name type changed to VARCHAR(128)

--- migrations/59_stale_materialized_view.up.sql
+++ migrations/59_stale_materialized_view.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE stale_materialized_view
 (
     id            BIGSERIAL PRIMARY KEY,
-    name          TEXT UNIQUE NOT NULL,
-    creation_date TIMESTAMP   NOT NULL
+    name          VARCHAR(128) UNIQUE NOT NULL,
+    creation_date TIMESTAMP           NOT NULL
 );
 
 CREATE INDEX stale_mv_creation_date_idx ON stale_materialized_view (creation_date);
