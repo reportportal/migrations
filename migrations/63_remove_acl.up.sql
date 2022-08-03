7dc8f9965bef5ba748c5c5b737984e3d3ce7194c
Pavel_Bortnik <pavel_bortnik@epam.com>
2022-08-03 16:08:03 +0300

    EPMRPP-78258 || Remove shareable entities

--- migrations/63_remove_acl.up.sql
+++ migrations/63_remove_acl.up.sql
@@ -7,4 +7,5 @@ DROP TABLE IF EXISTS acl_sid;
 ALTER TABLE shareable_entity DROP COLUMN shared;
 ALTER TABLE shareable_entity RENAME TO base_entity;
 
+DROP TRIGGER IF EXISTS after_widget_update ON widget;
 DROP FUNCTION IF EXISTS update_share_flag();
