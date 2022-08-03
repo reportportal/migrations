e50064ea213b550cf8f8f765ce4beba966c7868f
Andrei Piankouski <andrei.piankouski@ukg.com>
2022-08-03 12:42:04 +0300

    EPMRPP-78211 || Update DB with organizations

--- migrations/61_organization_tables.down.sql
+++ migrations/61_organization_tables.down.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS organization_attribute;
 
-DROP TABLE IF EXISTS organization;
-
 DROP TABLE IF EXISTS organization_user;
+
+DROP TABLE IF EXISTS organization;
 
 DROP TYPE IF EXISTS ORGANIZATION_ROLE_ENUM;
 
