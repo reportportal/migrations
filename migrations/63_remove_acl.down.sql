83ecdc34642dea171f654084b5efb0a1beee0ca8
Pavel_Bortnik <pavel_bortnik@epam.com>
2022-08-03 15:48:23 +0300

    EPMRPP-78258 || Remove acl tables

---
+++ migrations/63_remove_acl.down.sql
@@ -0,0 +1,46 @@
+CREATE TABLE acl_sid (
+    id        BIGSERIAL    NOT NULL PRIMARY KEY,
+    principal BOOLEAN      NOT NULL,
+    sid       VARCHAR(128) NOT NULL REFERENCES users (login) ON DELETE CASCADE,
+    CONSTRAINT unique_uk_1 UNIQUE (sid, principal)
+);
+
+CREATE INDEX acl_sid_idx
+    ON acl_sid (sid);
+
+CREATE TABLE acl_class (
+    id            BIGSERIAL    NOT NULL PRIMARY KEY,
+    class         VARCHAR(128) NOT NULL,
+    class_id_type VARCHAR(128),
+    CONSTRAINT unique_uk_2 UNIQUE (class)
+);
+
+CREATE TABLE acl_object_identity (
+    id                 BIGSERIAL PRIMARY KEY,
+    object_id_class    BIGINT      NOT NULL,
+    object_id_identity VARCHAR(36) NOT NULL,
+    parent_object      BIGINT,
+    owner_sid          BIGINT,
+    entries_inheriting BOOLEAN     NOT NULL,
+    CONSTRAINT unique_uk_3 UNIQUE (object_id_class, object_id_identity),
+    CONSTRAINT foreign_fk_1 FOREIGN KEY (parent_object) REFERENCES acl_object_identity (id),
+    CONSTRAINT foreign_fk_2 FOREIGN KEY (object_id_class) REFERENCES acl_class (id),
+    CONSTRAINT foreign_fk_3 FOREIGN KEY (owner_sid) REFERENCES acl_sid (id) ON DELETE CASCADE
+);
+
+CREATE TABLE acl_entry (
+    id                  BIGSERIAL PRIMARY KEY,
+    acl_object_identity BIGINT  NOT NULL,
+    ace_order           INT     NOT NULL,
+    sid                 BIGINT  NOT NULL,
+    mask                INTEGER NOT NULL,
+    granting            BOOLEAN NOT NULL,
+    audit_success       BOOLEAN NOT NULL,
+    audit_failure       BOOLEAN NOT NULL,
+    CONSTRAINT unique_uk_4 UNIQUE (acl_object_identity, ace_order),
+    CONSTRAINT foreign_fk_4 FOREIGN KEY (acl_object_identity) REFERENCES acl_object_identity (id) ON DELETE CASCADE,
+    CONSTRAINT foreign_fk_5 FOREIGN KEY (sid) REFERENCES acl_sid (id) ON DELETE CASCADE
+);
+
+ALTER TABLE base_entity ADD COLUMN shared BOOLEAN DEFAULT FALSE;
+ALTER TABLE base_entity RENAME TO shareable_entity;
---
+++ migrations/63_remove_acl.up.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS acl_entry;
+DROP TABLE IF EXISTS acl_object_identity;
+DROP TABLE IF EXISTS acl_class;
+DROP INDEX IF EXISTS acl_sid_idx;
+DROP TABLE IF EXISTS acl_sid;
+
+ALTER TABLE shareable_entity DROP COLUMN shared;
+ALTER TABLE shareable_entity RENAME TO base_entity;
