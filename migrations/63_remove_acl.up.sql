772f1a4457714f51067cc8e9cdfcf0ba050d47d0
Pavel_Bortnik <pavel_bortnik@epam.com>
2022-08-08 11:27:30 +0300

    EPMRPP-78258 || Drop share column

--- migrations/63_remove_acl.up.sql
+++ migrations/63_remove_acl.up.sql
@@ -5,6 +5,7 @@ DROP INDEX IF EXISTS acl_sid_idx;
 DROP TABLE IF EXISTS acl_sid;
 
 ALTER TABLE shareable_entity DROP COLUMN shared;
+ALTER TABLE dashboard_widget DROP COLUMN share;
 ALTER TABLE shareable_entity RENAME TO owned_entity;
 
 DROP TRIGGER IF EXISTS after_widget_update ON widget;
