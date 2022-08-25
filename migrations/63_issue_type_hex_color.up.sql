234975ccdaa7a0ae63135efc7c8c58e211b95b0e
Ivan_Kustau <Ivan_Kustau@epam.com>
2022-08-25 17:40:20 +0300

    EPMRPP-78986 || Updated issue type hex colors

---
+++ migrations/63_issue_type_hex_color.up.sql
@@ -0,0 +1,19 @@
+UPDATE issue_type
+SET hex_color = '#D32F2F'
+WHERE issue_name = 'Product Bug';
+
+UPDATE issue_type
+SET hex_color = '#FFC208'
+WHERE issue_name = 'Automation Bug';
+
+UPDATE issue_type
+SET hex_color = '#3E7BE6'
+WHERE issue_name = 'System Issue';
+
+UPDATE issue_type
+SET hex_color = '#76839B'
+WHERE issue_name = 'No Defect';
+
+UPDATE issue_type
+SET hex_color = '#00829B'
+WHERE issue_name = 'To Investigate';
