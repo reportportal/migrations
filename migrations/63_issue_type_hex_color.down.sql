3a723ce37ce3fbf247cd7f061145d877278e84e6
Ivan_Kustau <Ivan_Kustau@epam.com>
2022-09-08 13:04:03 +0300

    EPMRPP-79337 || Updated colors for sub issue types

--- migrations/63_issue_type_hex_color.down.sql
+++ migrations/63_issue_type_hex_color.down.sql
@@ -1,19 +1,19 @@
 UPDATE issue_type
 SET hex_color = '#ffb743'
-WHERE id = 1;
+WHERE issue_group_id = 1 AND hex_color = '#00829b';
 
 UPDATE issue_type
 SET hex_color = '#f7d63e'
-WHERE id = 2;
+WHERE issue_group_id = 2 AND hex_color = '#ffc208';
 
 UPDATE issue_type
 SET hex_color = '#ec3900'
-WHERE id = 3;
+WHERE issue_group_id = 3 AND hex_color = '#d32f2f';
 
 UPDATE issue_type
 SET hex_color = '#777777'
-WHERE id = 4;
+WHERE issue_group_id = 4 AND hex_color = '#76839b';
 
 UPDATE issue_type
 SET hex_color = '#0274d1'
-WHERE id = 5;
+WHERE issue_group_id = 5 AND hex_color = '#3e7be6';
--- migrations/63_issue_type_hex_color.up.sql
+++ migrations/63_issue_type_hex_color.up.sql
@@ -1,19 +1,19 @@
 UPDATE issue_type
 SET hex_color = '#00829b'
-WHERE id = 1;
+WHERE issue_group_id = 1 AND hex_color = '#ffb743';
 
 UPDATE issue_type
 SET hex_color = '#ffc208'
-WHERE id = 2;
+WHERE issue_group_id = 2 AND hex_color = '#f7d63e';
 
 UPDATE issue_type
 SET hex_color = '#d32f2f'
-WHERE id = 3;
+WHERE issue_group_id = 3 AND hex_color = '#ec3900';
 
 UPDATE issue_type
 SET hex_color = '#76839b'
-WHERE id = 4;
+WHERE issue_group_id = 4 AND hex_color = '#777777';
 
 UPDATE issue_type
 SET hex_color = '#3e7be6'
-WHERE id = 5;
+WHERE issue_group_id = 5 AND hex_color = '#0274d1';
