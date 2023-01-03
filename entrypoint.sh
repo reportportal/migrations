652dbae20d45b55bd3c44ec757bde24af9f61959
Ivan Kustau <86599591+IvanKustau@users.noreply.github.com>
2023-01-03 16:28:31 +0300

    EPMRPP-81336 || Update jenkins files (#241)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ if [ ! -z $POSTGRES_PASSWORD_FILE ]; then
   export POSTGRES_PASSWORD=$(cat $POSTGRES_PASSWORD_FILE)
 fi
 
-exec /wait-for-it.sh $POSTGRES_SERVER:$POSTGRES_PORT -- migrate -path /migrations -database postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER/$POSTGRES_DB?sslmode=$POSTGRES_SSLMODE "$@"
+exec /wait-for-it.sh $POSTGRES_SERVER:$POSTGRES_PORT -- migrate -path /migrations -database postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER:$POSTGRES_PORT/$POSTGRES_DB?sslmode=$POSTGRES_SSLMODE "$@"
