825ccea290084489b2e8a7ffa171add83359d0e1
Ivan_Kustau <Ivan_Kustau@epam.com>
2023-06-08 13:59:29 +0300

    EPMRPP-83651 || Remove delete cascade for activity

---
+++ migrations/73_user_activity_no_delete_cascade.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.activity
+    DROP CONSTRAINT IF EXISTS activity_user_id_fkey;
+
+ALTER TABLE public.activity
+    ADD FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE NO ACTION;
