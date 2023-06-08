11a2d039e8641cbce9b9631876f16a524ff20976
rkukharenka <ryhor_kukharenka@epam.com>
2023-06-08 10:55:41 +0300

    EPMRPP-84316-update-activity-table added migration script

---
+++ migrations/73_replace_activity_table.down.sql
@@ -0,0 +1,20 @@
+drop table if exists activity;
+
+create table activity
+(
+    id bigserial constraint activity_pk primary key,
+    user_id bigint,
+    username varchar,
+    project_id bigint,
+    entity varchar(128) not null,
+    action varchar(128) not null,
+    details jsonb null,
+    creation_date timestamp not null,
+    object_id bigint null
+);
+
+create index activity_project_idx on activity (project_id);
+
+create index activity_creation_date_idx on activity (creation_date);
+
+create index activity_object_idx on activity (object_id);
