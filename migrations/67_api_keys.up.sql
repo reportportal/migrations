981d5073305f2cbc589c8b7c94848047968142a7
Ivan Kustau <86599591+IvanKustau@users.noreply.github.com>
2023-06-19 15:03:12 +0300

    EPMRPP-84468 || Remove migration of old tokens from script (#252)

--- migrations/67_api_keys.up.sql
+++ migrations/67_api_keys.up.sql
@@ -9,10 +9,3 @@ CREATE TABLE IF NOT EXISTS api_keys (
 
 CREATE INDEX hash_api_keys_idx
     ON api_keys (hash);
-
-INSERT INTO api_keys (name, hash, created_at, user_id)
-SELECT 'Legacy API Key',
-       upper(encode(digest(token_id, 'sha3-256'), 'hex')),
-       now(),
-       user_id
-FROM oauth_access_token;
