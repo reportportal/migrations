a53ee531eb79b7a010f3fd96e009a8f3a4b3d0f0
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2024-02-06 13:16:16 +0300

    EPMRPP-89169 add organization tables (#291)

--- migrations/84_organization_tables.down.sql
+++ migrations/84_organization_tables.down.sql
@@ -1,3 +1,8 @@
+DROP INDEX IF EXISTS project_key_idx;
+ALTER TABLE project DROP COLUMN IF EXISTS "organization_id";
+ALTER TABLE project DROP COLUMN IF EXISTS "slug";
+ALTER TABLE project DROP COLUMN IF EXISTS "key";
+
 DROP INDEX IF EXISTS organization_slug_idx;
 
 DROP TABLE IF EXISTS organization_user;
