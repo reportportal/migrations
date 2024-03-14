1d8a2ee1dfb916e057ee83a7f80cbdbb8a4ef3d9
Andrei Piankouski <andrei_piankouski@epam.com>
2024-03-14 12:05:53 +0300

    EPMRPP-89659 || Add if exists

--- migrations/86_notication_update.down.sql
+++ migrations/86_notication_update.down.sql
@@ -1,10 +1,10 @@
-DROP INDEX unique_rule_name_per_project_rule_type;
+DROP INDEX IF EXISTS unique_rule_name_per_project_rule_type;
 
 CREATE UNIQUE INDEX unique_rule_name_per_project
     ON sender_case (rule_name, project_id);
 
 ALTER TABLE sender_case
-    DROP COLUMN rule_details;
+    DROP COLUMN IF EXISTS rule_details;
 
 ALTER TABLE sender_case
-    DROP COLUMN rule_type;
+    DROP COLUMN IF EXISTS rule_type;
