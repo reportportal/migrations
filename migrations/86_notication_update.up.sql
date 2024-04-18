c8451556ca38bdef2ae99c9d4af7ffc9afe62bd1
Andrei Piankouski <andrei_piankouski@epam.com>
2024-04-18 14:36:02 +0300

    EPMRPP-89659 || Add possibility to enable/disable group of notifications.

--- migrations/86_notication_update.up.sql
+++ migrations/86_notication_update.up.sql
@@ -8,3 +8,27 @@ DROP INDEX unique_rule_name_per_project;
 
 CREATE UNIQUE INDEX unique_rule_name_per_project_rule_type
     ON sender_case (rule_name, project_id, rule_type);
+
+DO $$
+DECLARE
+    new_attribute_id BIGINT;
+    old_attribute_id BIGINT;
+BEGIN
+    INSERT INTO attribute (name)
+    VALUES ('notifications.email.enabled')
+    RETURNING id INTO new_attribute_id;
+
+    SELECT id INTO old_attribute_id
+    FROM attribute
+    WHERE name = 'notifications.enabled';
+
+    INSERT INTO project_attribute (attribute_id, value, project_id)
+    SELECT new_attribute_id, value, project_id
+    FROM project_attribute
+    WHERE attribute_id = old_attribute_id;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE;
+END
+$$ LANGUAGE plpgsql;
