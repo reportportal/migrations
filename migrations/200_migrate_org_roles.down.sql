8dd11d0bef14951df15afa4744e33d2c90691d8e
siarhei_hrabko <siarhei_hrabko@epam.com>
2024-04-24 10:58:25 +0300

    EPMRPP-90205 Invitation links. Migration roles

--- migrations/200_migrate_org_roles.down.sql
+++ migrations/200_migrate_org_roles.down.sql
@@ -1,13 +1,18 @@
--- restore project roles from backup
-ALTER TABLE public.project_user DROP COLUMN IF EXISTS "project_role";
+-- revert user_creation_bid table
+ALTER TABLE public.user_creation_bid DROP COLUMN "organization_id";
+ALTER TABLE public.user_creation_bid DROP COLUMN "organization_role";
+ALTER TABLE public.user_creation_bid DROP COLUMN "role";
+ALTER TABLE public.user_creation_bid RENAME COLUMN "project_role_backup" TO "role";
+
+
+-- revert project roles and restore from backup
+ALTER TABLE public.project_user DROP COLUMN "project_role";
 DROP TYPE PROJECT_ROLE_ENUM;
 
 CREATE TYPE PROJECT_ROLE_ENUM AS ENUM ('OPERATOR', 'CUSTOMER', 'MEMBER', 'PROJECT_MANAGER');
 
 ALTER TABLE public.project_user ADD COLUMN "project_role" PROJECT_ROLE_ENUM;
-UPDATE public.project_user SET "project_role" = "project_role_backup"::project_role_enum
-    WHERE "project_role_backup" IS NOT NULL;
-
+UPDATE public.project_user SET "project_role" = "project_role_backup"::project_role_enum;
 ALTER TABLE public.project_user DROP COLUMN IF EXISTS "project_role_backup";
 
 
--- migrations/200_migrate_org_roles.up.sql
+++ migrations/200_migrate_org_roles.up.sql
@@ -2,6 +2,9 @@
 ALTER TABLE public.project_user ADD COLUMN "project_role_backup" TEXT;
 UPDATE public.project_user SET "project_role_backup" = "project_role";
 
+ALTER TABLE public.user_creation_bid  ADD COLUMN "project_role_backup" VARCHAR;
+UPDATE public.user_creation_bid SET "project_role_backup" = "role";
+
 
 -- Extend project roles
 ALTER TYPE project_role_enum RENAME TO PROJECT_ROLE_ENUM_OLD;
@@ -12,6 +15,7 @@ ALTER TABLE public.project_user
         USING (project_role::text::PROJECT_ROLE_ENUM);
 DROP TYPE PROJECT_ROLE_ENUM_OLD;
 
+
 -- Populate project 'key', 'slug' and 'organization_id'
 WITH org_id AS (INSERT INTO public.organization (name, slug, organization_type)
 VALUES ('My organization', 'my-organization', 'INTERNAL') RETURNING id)
@@ -20,6 +24,24 @@ SET "organization_id" = (SELECT id FROM org_id),
     "key" = slugify(name);
 
 UPDATE project SET "slug" = "key";
+
+
+-- Migrate User invitations
+ALTER TABLE public.user_creation_bid ADD COLUMN organization_id bigserial;
+ALTER TABLE public.user_creation_bid ADD COLUMN organization_role ORGANIZATION_ROLE_ENUM;
+
+UPDATE public.user_creation_bid SET organization_role = 'MEMBER'::public."organization_role_enum";
+
+UPDATE public.user_creation_bid SET role = 'EDITOR' WHERE "role" IN ('PROJECT_MANAGER', 'MEMBER', 'CUSTOMER');
+UPDATE public.user_creation_bid SET role = 'VIEWER' WHERE "role" = 'OPERATOR';
+
+UPDATE public.user_creation_bid AS ucb
+SET "organization_id" = (SELECT o.id FROM organization o WHERE o."name" = 'My organization');
+
+ALTER TABLE public.user_creation_bid ADD FOREIGN KEY (organization_id) REFERENCES organization (id) ON DELETE CASCADE;
+ALTER TABLE public.user_creation_bid ALTER COLUMN organization_id SET NOT NULL;
+ALTER TABLE public.user_creation_bid ALTER COLUMN organization_role SET NOT NULL;
+
 
 
 -- Update community projects:
@@ -53,6 +75,11 @@ ALTER TABLE public.project_user
 ALTER COLUMN project_role TYPE PROJECT_ROLE_ENUM_NEW
     USING (project_role::text::PROJECT_ROLE_ENUM_NEW);
 ALTER TABLE public.project_user ALTER COLUMN project_role SET NOT NULL;
+
+
+ALTER TABLE public.user_creation_bid
+    ALTER COLUMN "role" TYPE PROJECT_ROLE_ENUM_NEW
+        USING ("role"::text::PROJECT_ROLE_ENUM_NEW);
 
 DROP TYPE PROJECT_ROLE_ENUM;
 ALTER TYPE PROJECT_ROLE_ENUM_NEW RENAME TO PROJECT_ROLE_ENUM;
