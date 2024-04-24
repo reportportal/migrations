884df40d37c6527c8c3a4f03e520252cdb3331ef
Andrei Piankouski <andrei_piankouski@epam.com>
2024-04-24 13:49:35 +0300

    EPMRPP-89659 || Add possibility to enable/disable group of notifications.

--- migrations/86_notication_update.up.sql
+++ migrations/86_notication_update.up.sql
@@ -10,25 +10,17 @@ CREATE UNIQUE INDEX unique_rule_name_per_project_rule_type
     ON sender_case (rule_name, project_id, rule_type);
 
 DO $$
-DECLARE
-    new_attribute_id BIGINT;
-    old_attribute_id BIGINT;
 BEGIN
-    INSERT INTO attribute (name)
-    VALUES ('notifications.email.enabled')
-    RETURNING id INTO new_attribute_id;
-
-    SELECT id INTO old_attribute_id
-    FROM attribute
-    WHERE name = 'notifications.enabled';
-
-    INSERT INTO project_attribute (attribute_id, value, project_id)
-    SELECT new_attribute_id, value, project_id
-    FROM project_attribute
-    WHERE attribute_id = old_attribute_id;
-
-EXCEPTION
-    WHEN OTHERS THEN
-        RAISE;
+    IF NOT EXISTS (SELECT 1 FROM attribute WHERE name = 'notifications.email.enabled') THEN
+        WITH new_attr AS (
+            INSERT INTO attribute (name)
+            VALUES ('notifications.email.enabled')
+            RETURNING id
+        )
+        INSERT INTO project_attribute (attribute_id, value, project_id)
+        SELECT new_attr.id, value, project_id
+        FROM project_attribute, new_attr
+        WHERE attribute_id = (SELECT id FROM attribute WHERE name = 'notifications.enabled');
+    END IF;
 END
-$$ LANGUAGE plpgsql;
+$$;
