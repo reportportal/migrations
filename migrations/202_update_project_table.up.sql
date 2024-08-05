c883992128b400fbf97a7dd66dcf5cdb72074598
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2024-08-05 10:07:48 +0300

    EPMRPP-90223 update project and org tables (#315)
    * EPMRPP-93010 removed organizations feature scripts (#314)
    
    * EPMRPP-90223 update migration scripts
    
    * EPMRPP-90223 update project and org table
    
    * EPMRPP-90223 update project slug and key generation

--- migrations/202_update_project_table.up.sql
+++ migrations/202_update_project_table.up.sql
@@ -1,1 +1,3 @@
 ALTER TABLE project ADD COLUMN "updated_at" TIMESTAMP DEFAULT now() NOT NULL;
+ALTER TABLE project RENAME COLUMN "creation_date" TO "created_at";
+ALTER TABLE project DROP COLUMN IF EXISTS "project_type";
