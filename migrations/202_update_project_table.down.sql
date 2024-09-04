725db79f6ebf8203a564f2b9d9e0c7c0c2b36a01
siarhei_hrabko <siarhei_hrabko@epam.com>
2024-09-04 11:03:47 +0300

    EPMRPP-94686 project name unique per organization

--- migrations/202_update_project_table.down.sql
+++ migrations/202_update_project_table.down.sql
@@ -1,3 +1,5 @@
 ALTER TABLE project DROP COLUMN IF EXISTS "updated_at";
 ALTER TABLE project RENAME COLUMN "created_at" TO "creation_date";
 ALTER TABLE project ADD COLUMN project_type VARCHAR NOT NULL;
+
+ALTER TABLE project DROP CONSTRAINT project_name_unique_key;
--- migrations/202_update_project_table.up.sql
+++ migrations/202_update_project_table.up.sql
@@ -1,3 +1,5 @@
 ALTER TABLE project ADD COLUMN "updated_at" TIMESTAMP DEFAULT now() NOT NULL;
 ALTER TABLE project RENAME COLUMN "creation_date" TO "created_at";
 ALTER TABLE project DROP COLUMN IF EXISTS "project_type";
+
+ALTER TABLE project ADD CONSTRAINT project_name_unique_key UNIQUE ("name", "organization_id");
