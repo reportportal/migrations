ef611d405f55d85bde7d528310256df6dd6b5dfa
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2024-11-18 13:19:10 +0300

    EPMRPP-96226 bind organization and project tables (#324)

--- migrations/202_update_project_table.up.sql
+++ migrations/202_update_project_table.up.sql
@@ -3,3 +3,8 @@ ALTER TABLE project RENAME COLUMN "creation_date" TO "created_at";
 ALTER TABLE project DROP COLUMN IF EXISTS "project_type";
 
 ALTER TABLE project ADD CONSTRAINT project_name_unique_key UNIQUE ("name", "organization_id");
+
+ALTER TABLE project
+    ADD CONSTRAINT fk_project_organization FOREIGN KEY (organization_id)
+        REFERENCES organization (id)
+        ON DELETE CASCADE;
