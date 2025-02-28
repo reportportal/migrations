227c84c50d8651d6b8557ed115aa11128fbe0b6d
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2025-02-28 12:12:46 +0300

    Initial TMS migration: fixed "tms-milestone" and "tms-environment" table issue

--- migrations/94_tms_initial.down.sql
+++ migrations/94_tms_initial.down.sql
@@ -7,10 +7,7 @@ DROP TABLE IF EXISTS tms_manual_scenario;
 DROP TABLE IF EXISTS tms_test_case_version;
 DROP TABLE IF EXISTS tms_test_case;
 DROP TABLE IF EXISTS tms_test_folder;
-
--- Removing the ALTER statement on tms_environment table
-ALTER TABLE tms_environment DROP CONSTRAINT IF EXISTS tms_environment_fk_test_plan;
-
+DROP TABLE IF EXISTS tms_milestone;
 DROP TABLE IF EXISTS tms_test_plan;
 DROP TABLE IF EXISTS tms_environment;
 DROP TABLE IF EXISTS tms_product_version;
