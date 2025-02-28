20f32ee64dfa640f6c275293c5f05e3954a062b2
Pavel_Bortnik <pavel_bortnik@epam.com>
2025-02-28 19:08:45 +0300

    EPMRPP-98956 || Add key/value index

--- migrations/92_start_time_idx.down.sql
+++ migrations/92_start_time_idx.down.sql
@@ -1,1 +1,2 @@
 DROP INDEX IF EXISTS test_item_start_time_idx;
+DROP INDEX IF EXISTS item_attribute_key_value_idx;
--- migrations/92_start_time_idx.up.sql
+++ migrations/92_start_time_idx.up.sql
@@ -1,1 +1,2 @@
 CREATE INDEX IF NOT EXISTS test_item_start_time_idx ON test_item (start_time);
+CREATE INDEX IF NOT EXISTS item_attribute_key_value_idx ON item_attribute (key, value);
