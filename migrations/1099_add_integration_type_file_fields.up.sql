e102870775dad9cd12b8b61deb827186e6c96eb4
Reingold Shekhtel <raikbitters@gmail.com>
2025-04-10 19:16:33 +0200

    Add SQL scripts to update integration_type details and plugin names

---
+++ migrations/1099_add_integration_type_file_fields.up.sql
@@ -0,0 +1,8 @@
+UPDATE integration_type
+SET details = jsonb_set(details, '{details}', details->'details' ||
+    jsonb_build_object(
+      'fileId', details->'details'->'id',
+      'fileName', details->'details'->'name'
+    )
+)
+WHERE details->'details' ? 'id' AND details->'details' ? 'name';
