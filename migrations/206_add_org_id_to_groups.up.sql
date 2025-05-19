fd22769fc73ce2ddb79d9f73f73b29353b376f9a
raikbitters <raikbitters@gmail.com>
2025-05-19 19:12:05 +0200

    Fix foreign key reference in org_id column addition for groups table

--- migrations/206_add_org_id_to_groups.up.sql
+++ migrations/206_add_org_id_to_groups.up.sql
@@ -2,5 +2,5 @@ ALTER TABLE groups
     ADD COLUMN org_id INTEGER,
     ADD CONSTRAINT groups_org_id_fkey
     FOREIGN KEY (org_id)
-    REFERENCES organizations(id)
+    REFERENCES organization(id)
     ON DELETE CASCADE;
