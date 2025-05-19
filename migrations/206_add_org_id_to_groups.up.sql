9f13a25bfe3ea4f1789f5c0a0e2eb55fbc8ec773
raikbitters <raikbitters@gmail.com>
2025-05-19 18:58:14 +0200

    Make org_id column in groups table NOT NULL and update foreign key constraint name

--- migrations/206_add_org_id_to_groups.up.sql
+++ migrations/206_add_org_id_to_groups.up.sql
@@ -1,6 +1,6 @@
 ALTER TABLE groups
     ADD COLUMN org_id INTEGER,
-    ADD CONSTRAINT groups_org_id_fk
+    ADD CONSTRAINT groups_org_id_fkey
     FOREIGN KEY (org_id)
     REFERENCES organizations(id)
     ON DELETE CASCADE;
