5add2e6170683a4699b6e9fdbdac68afb94be401
siarhei_hrabko <siarhei_hrabko@epam.com>
2025-06-10 21:17:06 +0300

    EPMRPP-103880 || set project slug unique

--- migrations/202_update_project_table.down.sql
+++ migrations/202_update_project_table.down.sql
@@ -3,3 +3,4 @@ ALTER TABLE project RENAME COLUMN "created_at" TO "creation_date";
 ALTER TABLE project ADD COLUMN project_type VARCHAR NOT NULL;
 
 ALTER TABLE project DROP CONSTRAINT project_name_unique_key;
+ALTER TABLE project DROP CONSTRAINT project_slug_unique_key;
