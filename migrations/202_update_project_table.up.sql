1cdca5ab9290e6dd7e6895cbbe657311c5cd3086
Siarhei Hrabko <45555481+grabsefx@users.noreply.github.com>
2025-06-12 15:01:35 +0300

    EPMRPP-103880 || set project slug unique (#363)

--- migrations/202_update_project_table.up.sql
+++ migrations/202_update_project_table.up.sql
@@ -3,6 +3,7 @@ ALTER TABLE project RENAME COLUMN "creation_date" TO "created_at";
 ALTER TABLE project DROP COLUMN IF EXISTS "project_type";
 
 ALTER TABLE project ADD CONSTRAINT project_name_unique_key UNIQUE ("name", "organization_id");
+ALTER TABLE project ADD CONSTRAINT project_slug_unique_key UNIQUE ("slug", "organization_id");
 
 ALTER TABLE public.project ALTER COLUMN organization_id SET NOT NULL;
 ALTER TABLE project
