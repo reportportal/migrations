6bfc96e14b8f937a6d28e358977bf4a5fd23fa3a
H <36200539+imashkanov@users.noreply.github.com>
2026-03-04 12:21:01 +0300

    TMS Migration (to develop) (#367)
    * Initial TMS migration: fixed "tms-milestone" and "tms-environment" table issue
    
    * Initial TMS migration: added "project" foreign keys
    
    * Adjust DB schema for datasets
    
    * Added many-to-many between tms_environment and tms_dataset
    
    * Changed number from 95 to 96
    
    * Adjust DB schema for tms_test_case for 1 iteration
    
    * Design DB schema for manual test case execution
    
    * Update names of scripts
    
    * Update: retention_policy -> launch_type in script
    
    * Add priority to tms_test_case
    
    * Changed number from 102 to 103
    
    * Added full-text index for test-cases
    
    * Added full-text index for test-cases
    
    * Add relations between RPP-schema and TMS
    
    * Add external_id to tms_test_case
    
    * Make test case name unique in test folder
    
    * Split manual scenarios to STEPS and TEXT
    
    * Add created_at and updated_at columns to tms_test_case
    
    * Add many-to-many relationship for test-case <-> test plan
    
    * Remove many-to-many relationship: test-folder <-> test plan
    
    * Add full-text search to test-plan
    
    * Add tms-attachments
    
    * Add full text search filter condition enum value
    
    * Add full text search for tms attribute
    
    * Add tms_test_plan >-< launch relationship
    
    * Add test_case >-< launch
    
    * Add search vector for test_plan
    
    * Add test_plan_id to launch
    
    * Add test case execution
    
    * Add createdAt and updatedAt to tms_test_plan
    
    * Add not null to the name of tms_test_folder
    
    * Changed number of TMS scripts to 1000
    
    * Merge tms_test_case_execution and tms_test_case_launch
    
    * Fix tms_test_case_execution with launch_id
    
    * Remove tms_test_case_test_item (test_case_execution plays this role)
    
    * Add tms_manual_launch_attribute
    
    * Add tms_test_case_execution_comment and its attachment
    
    * Merge tms_step_test_item and tms_test_step_execution + enhances of tms_test_folder_test_item
    
    * Add bts ticket id column to tms test case execution
    
    * Remove tms_manual_launch_attribute (not required)
    
    * Added name and description to test_folder_test_item
    
    * Add TO_RUN status_enum
    
    * Remove non null contraint from tms step execution
    
    * Add priority to tms_test_case_execution
    
    * Make tms test case description's lenght == 1000 symbols
    
    * Add tms_milestone changes
    
    * Add number to tms step
    
    * Make tms-attribute project specific and avoid the usage of fts index that should work by the part of the key
    
    * Change instructions and expected_result type from varchar(255) to TEXT
    
    * Remove test_case name uniqueness in test folder
    
    * tms_test_case description varchar -> TEXT
    
    * tms attribute enhancements
    
    * enhance tms manual scenario requirements
    
    * Add number column to tms_manual_scenario_requirement for ordering
    
    * Add index column to tms_test_folder table
    
    * Add thumbnail_path to tms_attachment table in 1000_tms_initial.up.sql
    
    * Add thumbnail_path column to tms_attachment table
    
    * Add name to tms_test_case_execution
    
    ---------
    
    Co-authored-by: Konstantin_Shaplyko <konstantin_shaplyko@epam.com>

---
+++ migrations/1000_tms_initial.down.sql
@@ -0,0 +1,219 @@
+
+-- ============================================================================
+-- REVERT LAUNCH TABLE CHANGES
+-- ============================================================================
+
+ALTER TABLE launch DROP COLUMN IF EXISTS test_plan_id;
+ALTER TABLE launch DROP COLUMN IF EXISTS launch_type;
+
+-- ============================================================================
+-- DROP EXECUTION COMMENT ATTACHMENTS
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_execution_comment_attachment_attachment_id;
+DROP INDEX IF EXISTS idx_tms_execution_comment_attachment_comment_id;
+DROP TABLE IF EXISTS tms_test_case_execution_comment_attachment;
+
+-- ============================================================================
+-- DROP EXECUTION COMMENTS
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_test_case_execution_comment_execution_id;
+DROP TABLE IF EXISTS tms_test_case_execution_comment;
+
+-- ============================================================================
+-- DROP TEST CASE EXECUTION
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_test_case_execution_snapshot;
+DROP INDEX IF EXISTS idx_tms_test_case_execution_launch_case;
+DROP INDEX IF EXISTS idx_tms_test_case_execution_version_id;
+DROP INDEX IF EXISTS idx_tms_test_case_execution_launch_id;
+DROP INDEX IF EXISTS idx_tms_test_case_execution_test_item_id;
+DROP INDEX IF EXISTS idx_tms_test_case_execution_test_case_id;
+DROP TABLE IF EXISTS tms_test_case_execution;
+
+-- ============================================================================
+-- DROP ATTRIBUTE RELATIONSHIPS
+-- ============================================================================
+
+DROP TABLE IF EXISTS tms_test_plan_attribute;
+DROP TABLE IF EXISTS tms_test_case_attribute;
+DROP TABLE IF EXISTS tms_manual_scenario_attribute;
+
+-- ============================================================================
+-- DROP ATTACHMENT RELATIONSHIPS
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_preconditions_attachment_attachment_id;
+DROP INDEX IF EXISTS idx_preconditions_attachment_preconditions_id;
+DROP TABLE IF EXISTS tms_manual_scenario_preconditions_attachment;
+
+DROP INDEX IF EXISTS idx_tms_text_manual_scenario_attachment_attachment_id;
+DROP INDEX IF EXISTS idx_tms_text_manual_scenario_attachment_scenario_id;
+DROP TABLE IF EXISTS tms_text_manual_scenario_attachment;
+
+DROP INDEX IF EXISTS idx_tms_step_attachment_attachment_id;
+DROP INDEX IF EXISTS idx_tms_step_attachment_step_id;
+DROP TABLE IF EXISTS tms_step_attachment;
+
+-- ============================================================================
+-- DROP ATTACHMENTS
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_attachment_path;
+DROP INDEX IF EXISTS idx_tms_attachment_expires_at;
+DROP TABLE IF EXISTS tms_attachment;
+
+-- ============================================================================
+-- DROP STEP EXECUTION
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_step_execution_tms_step;
+DROP INDEX IF EXISTS idx_tms_step_execution_launch;
+DROP INDEX IF EXISTS idx_tms_step_execution_test_item;
+DROP INDEX IF EXISTS idx_tms_step_execution_test_case;
+DROP TABLE IF EXISTS tms_step_execution;
+
+-- ============================================================================
+-- DROP STEPS
+-- ============================================================================
+
+DROP TABLE IF EXISTS tms_step;
+
+-- ============================================================================
+-- DROP MANUAL SCENARIO TYPES
+-- ============================================================================
+
+DROP TABLE IF EXISTS tms_steps_manual_scenario;
+DROP TABLE IF EXISTS tms_text_manual_scenario;
+
+-- ============================================================================
+-- DROP MANUAL SCENARIO PRECONDITIONS
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_manual_scenario_preconditions_scenario_unique;
+DROP TABLE IF EXISTS tms_manual_scenario_preconditions;
+
+-- ============================================================================
+-- DROP MANUAL SCENARIO REQUIREMENTS
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_manual_scenario_requirement_scenario_id;
+DROP TABLE IF EXISTS tms_manual_scenario_requirement;
+
+-- ============================================================================
+-- DROP MANUAL SCENARIO
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_manual_scenario_type;
+DROP TABLE IF EXISTS tms_manual_scenario;
+
+-- ============================================================================
+-- DROP TEST CASE VERSION
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_test_case_version_default;
+DROP TABLE IF EXISTS tms_test_case_version;
+
+-- ============================================================================
+-- DROP TEST PLAN - TEST CASE (Many-to-Many)
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_test_plan_test_case_test_case_id;
+DROP INDEX IF EXISTS idx_tms_test_plan_test_case_test_plan_id;
+DROP TABLE IF EXISTS tms_test_plan_test_case;
+
+-- ============================================================================
+-- DROP TEST CASE
+-- ============================================================================
+
+DROP TRIGGER IF EXISTS tms_test_case_search_vector_trigger ON tms_test_case;
+DROP FUNCTION IF EXISTS update_tms_test_case_search_vector();
+DROP INDEX IF EXISTS idx_tms_test_case_search_vector;
+DROP TABLE IF EXISTS tms_test_case;
+
+-- ============================================================================
+-- DROP TEST FOLDER TEST ITEM
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_test_folder_test_item_test_item_id;
+DROP INDEX IF EXISTS idx_tms_test_folder_test_item_test_folder_id;
+DROP TABLE IF EXISTS tms_test_folder_test_item;
+
+-- ============================================================================
+-- DROP TEST FOLDER
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_test_folder_project_id;
+DROP TABLE IF EXISTS tms_test_folder;
+
+-- ============================================================================
+-- DROP TEST PLAN
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_test_plan_milestone_id;
+DROP INDEX IF EXISTS idx_tms_test_plan_project_id;
+DROP INDEX IF EXISTS idx_tms_test_plan_search_vector;
+DROP TRIGGER IF EXISTS tms_test_plan_search_vector_trigger ON tms_test_plan;
+DROP FUNCTION IF EXISTS update_tms_test_plan_search_vector();
+DROP TABLE IF EXISTS tms_test_plan;
+
+-- ============================================================================
+-- DROP MILESTONE
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_milestone_product_version_id;
+DROP INDEX IF EXISTS idx_tms_milestone_project_id;
+DROP TABLE IF EXISTS tms_milestone;
+
+-- ============================================================================
+-- DROP ENVIRONMENT
+-- ============================================================================
+
+DROP TABLE IF EXISTS tms_environment_dataset;
+DROP TABLE IF EXISTS tms_environment;
+
+-- ============================================================================
+-- DROP DATASET
+-- ============================================================================
+
+DROP TABLE IF EXISTS tms_dataset_data;
+DROP TABLE IF EXISTS tms_dataset;
+
+-- ============================================================================
+-- DROP PRODUCT VERSION
+-- ============================================================================
+
+DROP TABLE IF EXISTS tms_product_version;
+
+-- ============================================================================
+-- DROP ATTRIBUTE
+-- ============================================================================
+
+DROP INDEX IF EXISTS idx_tms_attribute_project_value;
+DROP INDEX IF EXISTS idx_tms_attribute_value_trgm;
+DROP INDEX IF EXISTS idx_tms_attribute_project_key;
+DROP INDEX IF EXISTS idx_tms_attribute_key_trgm;
+DROP INDEX IF EXISTS idx_tms_attribute_project_id;
+DROP TABLE IF EXISTS tms_attribute;
+
+-- ============================================================================
+-- DROP ENUMS
+-- ============================================================================
+
+DROP TYPE IF EXISTS LAUNCH_TYPE_ENUM;
+DROP TYPE IF EXISTS tms_manual_scenario_type;
+DROP TYPE IF EXISTS tms_milestone_type;
+DROP TYPE IF EXISTS tms_milestone_status;
+DROP TYPE IF EXISTS tms_dataset_type;
+
+-- ============================================================================
+-- DROP EXTENSIONS (optional)
+-- ============================================================================
+
+-- DROP EXTENSION IF EXISTS pg_trgm;
+
+-- ============================================================================
+-- END OF DROP SCRIPT
+-- ============================================================================
