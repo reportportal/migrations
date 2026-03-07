c3d577286d5c3f81c836759a4d18c013344cdca7
Pavel_Bortnik <pavel_bortnik@epam.com>
2026-03-07 19:07:50 +0300

    EPMRPP-112329 || Cleanup schema in scripts

--- migrations/115_remove_gist_index_on_path.down.sql
+++ migrations/115_remove_gist_index_on_path.down.sql
@@ -1,3 +1,3 @@
-CREATE INDEX path_gist_idx ON public.test_item USING gist (path);
+CREATE INDEX path_gist_idx ON test_item USING gist (path);
 
 DROP INDEX IF EXISTS test_item_path_idx;
--- migrations/6_retries_handling.up.sql
+++ migrations/6_retries_handling.up.sql
@@ -299,14 +299,14 @@ BEGIN
         RETURN new;
     END IF;
 
-    defect_field := (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$',
-                                   lower(public.issue_type.locator))
+    defect_field := (SELECT concat('statistics$defects$', lower(issue_group.issue_group :: VARCHAR), '$',
+                                   lower(issue_type.locator))
                      FROM issue
                               JOIN issue_type ON issue.issue_type = issue_type.id
                               JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
                      WHERE issue.issue_id = new.issue_id);
 
-    defect_field_total := (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$total')
+    defect_field_total := (SELECT concat('statistics$defects$', lower(issue_group.issue_group :: VARCHAR), '$total')
                            FROM issue
                                     JOIN issue_type ON issue.issue_type = issue_type.id
                                     JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
@@ -404,8 +404,8 @@ BEGIN
         RETURN new;
     END IF;
 
-    defect_field := (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$',
-                                   lower(public.issue_type.locator))
+    defect_field := (SELECT concat('statistics$defects$', lower(issue_group.issue_group :: VARCHAR), '$',
+                                   lower(issue_type.locator))
                      FROM issue_type
                               JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
                      WHERE issue_type.id = new.issue_type);
@@ -413,13 +413,13 @@ BEGIN
     defect_field_old_id := (SELECT DISTINCT ON (statistics_field.name) sf_id
                             FROM statistics_field
                             WHERE statistics_field.name =
-                                  (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$',
-                                                 lower(public.issue_type.locator))
+                                  (SELECT concat('statistics$defects$', lower(issue_group.issue_group :: VARCHAR), '$',
+                                                 lower(issue_type.locator))
                                    FROM issue_type
                                             JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
                                    WHERE issue_type.id = old.issue_type));
 
-    defect_field_total := (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$total')
+    defect_field_total := (SELECT concat('statistics$defects$', lower(issue_group.issue_group :: VARCHAR), '$total')
                            FROM issue_type
                                     JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
                            WHERE issue_type.id = new.issue_type);
@@ -427,7 +427,7 @@ BEGIN
     defect_field_old_total_id := (SELECT DISTINCT ON (statistics_field.name) sf_id
                                   FROM statistics_field
                                   WHERE statistics_field.name =
-                                        (SELECT concat('statistics$defects$', lower(public.issue_group.issue_group :: VARCHAR), '$total')
+                                        (SELECT concat('statistics$defects$', lower(issue_group.issue_group :: VARCHAR), '$total')
                                          FROM issue_type
                                                   JOIN issue_group ON issue_type.issue_group_id = issue_group.issue_group_id
                                          WHERE issue_type.id = old.issue_type));
