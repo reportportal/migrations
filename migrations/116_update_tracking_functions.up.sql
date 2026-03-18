49688f8d95723690c7fff4ddaf6a659fff7c1c8c
Pavel_Bortnik <pavel_bortnik@epam.com>
2026-03-18 16:21:56 +0300

    EPMRPP-113578 || Update script for tracking changes

---
+++ migrations/116_update_tracking_functions.up.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION public.update_last_modified_from_item_attribute()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+  BEGIN
+      IF NEW.item_id IS NOT NULL OR OLD.item_id IS NOT NULL THEN
+          UPDATE test_item
+          SET last_modified = CURRENT_TIMESTAMP
+          WHERE item_id = COALESCE(NEW.item_id, OLD.item_id);
+      END IF;
+
+      IF NEW.launch_id IS NOT NULL OR OLD.launch_id IS NOT NULL THEN
+          UPDATE launch
+          SET last_modified = CURRENT_TIMESTAMP
+          WHERE id = COALESCE(NEW.launch_id, OLD.launch_id);
+      END IF;
+
+      RETURN NEW;
+  END;
+  $function$
+;
