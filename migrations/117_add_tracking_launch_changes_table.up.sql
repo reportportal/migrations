68dc9693b8ae20ce76a99c52008cc94ea3d8e1ee
Pavel_Bortnik <pavel_bortnik@epam.com>
2026-04-14 16:32:31 +0300

    EPMRPP-113576 || Update scripts

--- migrations/117_add_tracking_launch_changes_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS launches_modified
-(
-    launch_id  BIGINT PRIMARY KEY REFERENCES launch (id) ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL DEFAULT clock_timestamp()
-);
-
-CREATE INDEX IF NOT EXISTS idx_launches_modified_created_at ON launches_modified (created_at);
