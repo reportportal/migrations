a4ad2366ecbbbc2bff40807f74bfbb77eb15bff4
siarhei_hrabko <siarhei_hrabko@epam.com>
2026-08-13 10:54:45 +0300

    EPMRPP-118518 || pre-release scripts refactoring

--- migrations/120_log_item_level_idx.down.sql
+++ migrations/120_log_item_level_idx.down.sql
@@ -1,3 +1,1 @@
 DROP INDEX IF EXISTS log_item_level_idx;
-
-CREATE INDEX log_ti_idx ON log (item_id);
--- migrations/120_log_item_level_idx.up.sql
+++ migrations/120_log_item_level_idx.up.sql
@@ -1,3 +1,1 @@
 CREATE INDEX IF NOT EXISTS log_item_level_idx ON log (item_id, log_level);
-
-DROP INDEX IF EXISTS log_ti_idx;
