f16e0cce8f92c86b6ce020eaf5e02ba1e055c71d
Hleb Mashkanov <gleb.mashkanov@gmail.com>
2026-09-13 12:38:58 +0300

    EPMRPP-120613: Add indexes for manual launch creation performance
    + add down migration for manual launch performance indexes

--- migrations/223_manual_launch_performance_indexes.down.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS idx_tms_test_folder_test_item_launch_folder;
-DROP INDEX IF EXISTS idx_launch_test_plan_id;
--- migrations/223_manual_launch_performance_indexes.up.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_tms_test_folder_test_item_launch_folder ON tms_test_folder_test_item (launch_id, test_folder_id);
-CREATE INDEX IF NOT EXISTS idx_launch_test_plan_id ON launch (test_plan_id) WHERE test_plan_id IS NOT NULL;
